7cd9ad53af3ee7088ac0c8abe56ddb869bd2bf27
bhari eutsey <bharilynn@outlook.com>
2022-01-23 14:54:51 -0600

    attempting to get route for notes.html, stackover flow tip 'path.resolve doesn't work

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// MIDDLEWARE FUNCTION TO INCOMING PARSE DATA
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+
 // GENERATE UNIQUE ID FROM NPM INSTALLATION
 const generateUniqueId = require('generate-unique-id');
 
@@ -10,10 +14,10 @@ const generateUniqueId = require('generate-unique-id');
 
 // GET ROUTE FOR NOTES.HTML
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.resolve('./public/index.html'));// path.resolve(from stack overflow) returns 'path not defined'
 });
 
-// MAKE THE SERVER LISTEN
+// MAKE THE SERVER LISTEN (WITH DIRECT LINK)
 app.listen(3001, () => {
     console.log('API server now on port 3001 http://localhost:3001');
 });
