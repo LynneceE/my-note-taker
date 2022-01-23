7009c4ec2609bc89fbb4af4a7594a3a28b7d77be
bhari eutsey <bharilynn@outlook.com>
2022-01-22 23:17:09 -0600

    added local host link to console log, started get route to return 'hello world' to ensure it works, installed generate-unique-id through npm

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // SET UP EXPRESS
-
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// GENERATE UNIQUE ID FROM NPM INSTALLATION
+const generateUniqueId = require('generate-unique-id');
+
+
+
+// GET ROUTE FOR NOTES.HTML
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 // MAKE THE SERVER LISTEN
 app.listen(3001, () => {
-    console.log('API server now on port 3001');
+    console.log('API server now on port 3001 http://localhost:3001');
 });
+
+
+
+
+
+
