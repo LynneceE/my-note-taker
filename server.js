c1d9b1d6fc26187cf4f0cdf2df88a9623e9443a0
bhari eutsey <bharilynn@outlook.com>
2022-01-23 21:39:00 -0600

    added notes const to require db.json

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const path = require('path');
+//const PORT = process.env.PORT || 3001;
+
+// GET NOTES FROM DB.JSON
+const { notes } = require('./db/db.json'); 
 
 // MIDDLEWARE FUNCTION TO INCOMING PARSE DATA
 app.use(express.urlencoded({ extend: true }));
@@ -10,12 +15,14 @@ app.use(express.json());
 // GENERATE UNIQUE ID FROM NPM INSTALLATION
 const generateUniqueId = require('generate-unique-id');
 
-
+// CREATING A NEW NOTE
+function newNote()
 
 // GET ROUTE FOR NOTES.HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve('./public/index.html'));// path.resolve(from stack overflow) returns 'path not defined'
+app.get('./public/index.html',(req, res) => {
+    res.sendFile(path.resolve(__dirname,'./public/index.html'));// path.resolve(from stack overflow) returns 'path not defined'
 });
+
 
 // MAKE THE SERVER LISTEN (WITH DIRECT LINK)
 app.listen(3001, () => {
